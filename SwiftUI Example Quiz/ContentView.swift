44ebbb4d61c149827149ba29b88740b730cb2e7b
Garrit <garrit.schaap@ju.se>
2021-11-04 09:01:44 +0100

    Initial Commit

---
+++ SwiftUI Example Quiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Example Quiz
+//
+//  Created by Garrit Schaap on 2021-11-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Example Quiz/SwiftUI_Example_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Example_QuizApp.swift
+//  SwiftUI Example Quiz
+//
+//  Created by Garrit Schaap on 2021-11-04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Example_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
